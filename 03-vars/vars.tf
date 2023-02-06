7a45965c7f43387ac9ce299f7b975732ba23857f
ChaitanyaKottha <chythanya.k@gmail.com>
2023-02-07 02:26:16 +0530

    updating learn-terraform

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -5,3 +5,24 @@ variable "sample" {
 output "sample" {
   value = var.sample
 }
+
+# Default variable types - string datatype
+varaible "sample1"{
+  default = "Hello"
+}
+# number datatype
+variable "sample2" {
+  default = 100
+}
+# boolean datatype
+variable "sample3" {
+  default = true
+}
+# list varaibale type
+ variable "sample5" {
+   default = [
+     100,
+     "abc",
+     true
+   ]
+ }
