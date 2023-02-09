7470372b225ff2fd05befb8276ee274ef6bff3c7
ChaitanyaKottha <chythanya.k@gmail.com>
2023-02-10 02:29:57 +0530

    updating learn-terraform

--- 08-loops/for_each_module/Modules/main.tf
+++ 08-loops/for_each_module/Modules/main.tf
@@ -13,26 +13,28 @@ data "aws_ami" "centos8" {
   name_regex = "Centos-8-DevOps-Practice"
   owners = ["973714476881"]
 }
-
-output "public_ip" {
-  #without count
-  #value = aws_instance.test.public_ip
-  #with count
-  value = {
-    for k, v in aws_instance.test : k => v.public_ip
-  }
-}
-#map
-variable "components" {
-  default = {
-    cart = {
-      name = "cart",
-      instance_type = "t3.micro"
-    }
-    catalogue = {
-      name = "catalogue",
-      instance_type = "t3.small"
-    }
-  }
-
-}
+#
+#output "public_ip" {
+#  #without count
+#  #value = aws_instance.test.public_ip
+#  #with count
+#  value = {
+#    for k, v in aws_instance.test : k => v.public_ip
+#  }
+#}
+##map
+#variable "components" {
+#  default = {
+#    cart = {
+#      name = "cart",
+#      instance_type = "t3.micro"
+#    }
+#    catalogue = {
+#      name = "catalogue",
+#      instance_type = "t3.small"
+#    }
+#  }
+#
+#}
+variable "instance_type" {}
+variable "name" {}
