0e60b8c529fc5428465c835c90e455612442caab
ChaitanyaKottha <chythanya.k@gmail.com>
2023-02-10 03:33:04 +0530

    updating learn-terraform

--- 10-local/main.tf
+++ 10-local/main.tf
@@ -14,8 +14,13 @@ variable "components" {
 
 locals {
   instance_type = { for k, v in var.components : k => v.instance_type}
+  name = { for k, v in var.components : k => v.name}
 }
 
 output "instance_type" {
   value = local.instance_type
+}
+
+output "name" {
+  value = local.name
 }
